a971b0ba2742203ecc20df0cc45af841ea979e8e
Paul Muraru <paulmuraru2004@gmail.com>
2023-12-26 15:07:04 -0500

    Add a register method in ModItems

--- src/main/java/net/rainworld/rainworldbiomes/item/ModItems.java
+++ src/main/java/net/rainworld/rainworldbiomes/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.rainworld.rainworldbiomes.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.rainworld.rainworldbiomes.RainWorldBiomes;
@@ -9,5 +10,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = 
         DeferredRegister.create(ForgeRegistries.ITEMS, RainWorldBiomes.MODID);
 
-    
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
