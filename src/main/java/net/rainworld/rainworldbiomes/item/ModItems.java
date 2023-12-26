5a8540c5bf36be6b6f7d5d0899335ff494f41888
Paul Muraru <paulmuraru2004@gmail.com>
2023-12-26 15:05:58 -0500

    Adding ModItems deferred registry

---
+++ src/main/java/net/rainworld/rainworldbiomes/item/ModItems.java
@@ -0,0 +1,13 @@
+package net.rainworld.rainworldbiomes.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.rainworld.rainworldbiomes.RainWorldBiomes;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = 
+        DeferredRegister.create(ForgeRegistries.ITEMS, RainWorldBiomes.MODID);
+
+    
+}
