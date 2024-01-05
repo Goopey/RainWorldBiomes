e8fbd17e766071c4a49e9c3e313d7f1fd2e89ebe
Paul Muraru <paulmuraru2004@gmail.com>
2024-01-04 22:31:48 -0500

    Add BubbleFruitItem skeleton class

---
+++ src/main/java/net/rainworld/rainworldbiomes/item/custom/BubbleFruitItem.java
@@ -0,0 +1,16 @@
+package net.rainworld.rainworldbiomes.item.custom;
+
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+
+public class BubbleFruitItem extends Item {
+    public BubbleFruitItem(Properties properties) {
+        super(properties);
+    }
+
+    
+}
